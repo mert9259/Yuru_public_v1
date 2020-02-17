9c6eabd1eb9cfc4fc280f8d3254c24d73132e5a7
MERT <mert9259@gmail.com>
2020-02-17 23:10:09 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const userRoutes = require("./routes/users");
+
+//Routes
+const usersRoutes = require("./routes/users");
+const sessionsRoutes = require("./routes/sessions");
+const authenticationRoutes = require("./controller/authentication");
+const stepsRoutes = require("./routes/steps");
+const couponsRoutes = require("./routes/coupons");
+const prizesRoutes = require("./routes/prizes");
 
 
 
 var app = express();
 app.use(bodyParser.json());
 
-app.use("/users",userRoutes);
 
 
-app.listen(80);
+
+app.use("/sessions",sessionsRoutes);
+app.use(authenticationRoutes.router);
+app.use("/steps",stepsRoutes);
+app.use("/users",usersRoutes);
+app.use("/coupons",couponsRoutes);
+app.use("/prizes",prizesRoutes);
+
+
+//Yönetici kontrolleri ve fonksiyonları bu bölümde olmalıdır.
+
+
+app.listen(80,()=>console.log("Sever started on port 80"));
